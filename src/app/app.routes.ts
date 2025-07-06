440c81394b47858b9e5ee95a038495e1f8eea2e1
roxana-hgh <haghgoo.roxana@gmail.com>
2025-07-06 14:16:29 +0330

    login signup ui added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,17 +1,21 @@
 import { Routes } from '@angular/router';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { BaseComponent } from './components/base/base.component';
+import { LoginComponent } from './components/auth/login/login.component';
+import { SignupComponent } from './components/auth/signup/signup.component';
 
 export const routes: Routes = [
-    {
+  {
     path: '',
     component: BaseComponent,
     children: [
       { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
       {
         path: 'dashboard',
-        component: DashboardComponent
-      }
-    ]
-}
+        component: DashboardComponent,
+      },
+    ],
+  },
+  { path: 'login', component: LoginComponent },
+  { path: 'signup', component: SignupComponent },
 ];
